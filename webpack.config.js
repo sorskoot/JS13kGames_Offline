f44dd26fb62de484ebd8ee743af16ba5a36211b2
Timmy Kokke <mental_c@hotmail.com>
2018-08-22 23:50:52 +0200

    added stuff

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "mode": "development",
+    "entry": "./src/index.js",
+    "output": {
+        "path": __dirname + '/dist',
+        "filename": "[name].js"
+    },
+    "devtool": "inline-source-map",
+    "module": {
+        "rules": [
+            {
+                "enforce": "pre",
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                //     "use": "jshint-loader"
+            },
+            {
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env"
+                        ]
+                    }
+                }
+            },
+            {
+                "test": /\.scss$/,
+                "use": [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+}
